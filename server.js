c8c77ca2125f575f6a7dfeda72503c8fea1ceda0
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-03-01 19:07:03 +0100

    test

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-	res.send("Node.js project.");
+app.post("/submit-form", (req, res) => {
+	const { name, email, message } = req.body;
+	console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+	res.send("Form submitted successfully!");
 });
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const mongoose = require("mongoose");
+
+const mongoose = require("mongoose");
+const uri = "yourMongoDBAtlasConnectionURIHere";
+
+mongoose
+	.connect(uri)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("MongoDB connection error:", err));
